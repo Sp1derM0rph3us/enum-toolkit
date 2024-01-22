c1a46be88745a8dfe643f1659643b924ca61c42a
Sp1derM0rph3us <joao.v.aion@gmail.com>
2024-01-21 23:21:47 -0300

    Updated bgrabber.py

--- bgrabber.py
+++ bgrabber.py
@@ -2,12 +2,14 @@
 # Simple banner grabber in python
 # Made by: Sp1d3rM0rph3us
 
-import socket, sys, ssl
+import socket, sys, ssl, time
 
 common_http_ports = [80, 8080, 10000]
 common_https_ports = [443, 8443]
+user_list = ['root', 'sys', 'www-data', 'mail']
 
 def http_banner_graber(target, p):
+    tcp = None
     try:
         tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         tcp.connect((target, p))
@@ -20,7 +22,8 @@ def http_banner_graber(target, p):
     except Exception as e:
         print("Error: ", e)
     finally:
-        tcp.close()
+        if tcp:
+            tcp.close()
 
 def https_banner_graber(target, p):
     try:
@@ -44,7 +47,29 @@ def https_banner_graber(target, p):
         if ssl_socket:
             ssl_socket.close()
 
+def smtp_banner_grabber(target, p):
+    tcp = None
+    try:
+        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp.connect((target, p))
+        time.sleep(1.5)
+        
+        # Creating the request
+        for user in user_list:
+            request = f"VRFY {user}\r\n"
+            tcp.send(request.encode())
+            banner = tcp.recv(1024).decode('utf-8').strip()
+            print(banner)
+    except Exception as e:
+        print("Error: ", e)
+
+    finally:
+        if tcp:
+            tcp.close()
+
+
 def banner_graber(target, p):
+    tcp = None
     try:
         tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         tcp.connect((target, p))
@@ -54,7 +79,8 @@ def banner_graber(target, p):
     except Exception as e:
             print("Error: ", e)
     finally:
-        tcp.close()
+        if tcp:
+            tcp.close()
 
 
 ## Checking if arguments were given ##
@@ -74,6 +100,9 @@ else:
     elif p in common_https_ports:
         print(f"[+] Banner for {target}:{p}")
         https_banner_graber(target, p)
+    elif p == 25:
+        print(f"[+] Banner for {target}:{p}")
+        smtp_banner_grabber(target, p)
     else:
         print(f"[+] Banner for {target}:{p}")
         banner_graber(target,p)
